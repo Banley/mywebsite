104c527125c3ee703339faec2c62355d91641aa0
Banley <thomas@banley.com>
2014-11-08 22:04:46 -0600

    added some sweeto animations

--- hello.js
+++ hello.js
@@ -1,12 +1,25 @@
 
 document.getElementById("msg").textContent = "Hello from Javascript!";
 
-var x = 0;
-
+var x = 1;
+var y = 1;
+var dx = 1;
+var dy = 1;
+var scale = .5;
+var dscale = .05;
 setInterval(animate, 50);
 
 function animate() {
-	x = x + 1;
-	document.getElementById("msg").style.top = x + "px";
+	if (x > 150) dx = -1;
+	if (x < 1) dx = 1;
+	if (y > 100) dy = -1;
+	if (y < 1) dy = 1;
+	if (scale < .5) dscale = .05;
+	if (scale > 2) dscale = -.05;
+	x = x + dx;
+	y = y + dy;
+	scale = scale + dscale;
+	document.getElementById("msg").style.top = y + "px";
 	document.getElementById("msg").style.left = x + "px";
+	document.getElementById("msg").style.transform = "scale(" + scale + ", " + scale + ")";
 }
